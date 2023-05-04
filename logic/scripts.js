27b2e46688d232a143f5a7ccf50acfc3c655d606
dnh <rootdnh@gmail.com>
2023-05-04 19:00:21 -0300

    finish project

--- logic/scripts.js
+++ logic/scripts.js
@@ -1,5 +1,25 @@
-const changeThemeBtn = document.querySelector('#change-theme');
+const changeThemeBtn = document.querySelector("#change-theme");
 
-changeThemeBtn.addEventListener("change", function (){
-  document.body.classList.toggle("dark");
-})
+function toggleDarkMode() {
+ document.body.classList.toggle("dark");
+}
+
+function loadTheme() {
+ const darMode = localStorage.getItem("dark");
+
+ if (darMode) {
+  toggleDarkMode();
+ }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+ toggleDarkMode();
+
+ localStorage.removeItem("dark");
+
+ if (document.body.classList.contains("dark", 1)) {
+  localStorage.setItem("dark", 1);
+ }
+});
